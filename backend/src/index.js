f203af7e96597bfe49423b5eed513ad5446f49bb
Upayan Naskar <upayannaskar2009@outlook.com>
2025-02-22 14:17:58 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+import { connectDB } from "./lib/db.js"
+
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+    connectDB()
+})
