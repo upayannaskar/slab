63001b88dffb09d7f7b25e75d8635603188457bf
Upayan Naskar <upayannaskar2009@outlook.com>
2025-03-12 10:24:04 +0530

    index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ import {app, server} from "./lib/socket.js"
 
 dotenv.config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
