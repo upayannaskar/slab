655a2153496e321738c3fff9f784ed0808b1ea87
Upayan Naskar <upayannaskar2009@outlook.com>
2025-03-12 11:53:44 +0530

    final and the best

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ limit: '20mb', extended: true }));
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://slab-beta.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
