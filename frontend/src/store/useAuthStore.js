5ce1bc5289a51efd73fbe99ba38f24e2fe4be4be
Upayan Naskar <150513312+upayannaskar@users.noreply.github.com>
2025-03-12 09:50:32 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import {axiosInstance} from "../lib/axios"
 import toast from "react-hot-toast"
 import {io} from "socket.io-client"
 
-const BASE_URL = "https://slab-backend.onrender.com"
+const BASE_URL = "https://slab-backend.onrender.com/api"
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
