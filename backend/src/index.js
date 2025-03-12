fd416fd1548a7733b7512349d857b7950ca92acd
Upayan Naskar <upayannaskar2009@outlook.com>
2025-03-12 14:51:06 +0530

    Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ limit: '20mb', extended: true }));
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://slab-beta.vercel.app",
     credentials: true
 }))
 
