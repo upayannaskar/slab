49a0dd8c255192db311a75c66f86c43ed6a49308
Upayan Naskar <upayannaskar2009@outlook.com>
2025-03-13 11:25:10 +0530

    Increased Payload size limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,9 @@ app.use(
   })
 );
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
